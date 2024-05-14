18965b6efa23b6a698e1b9448a57d7e391920ebd
DevWithEasy <devwitheasy@gmail.com>
2024-05-14 19:45:56 +0600

    tailwind css add

--- app/index.js
+++ app/index.js
@@ -1,10 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text,Image, StatusBar, ScrollView } from 'react-native'
 import React from 'react'
+import doctor_image from '../assets/doctor_male.png'
+
 
 export default function index() {
     return (
-        <View>
-            <Text>index</Text>
+        <ScrollView
+            className = 'bg-white mt-10 p-4'
+        >
+            <StatusBar />
+            <View
+                className = 'w-1/2 justify-center items-center border p-2 rounded'
+            >
+            <Image 
+                source={doctor_image} 
+                style={{ width: 100, height: 100 }} 
+            />
+            <Text className='text-blue-500'>Dr. Abedur Rahman</Text>
+            <Text>Rangpur Medical Collage</Text>
         </View>
+        </ScrollView>
+        
     )
 }
